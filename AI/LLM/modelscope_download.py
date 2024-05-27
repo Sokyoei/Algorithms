89389fcb0cb40d74824892acd74c6a2e3d77bbb6
Sokyoei <89757286+Sokyoei@users.noreply.github.com>
2024-05-27 09:57:15 +0800

    move NLP and LLM to Sokyoei/NLP

--- AI/LLM/modelscope_download.py
+++
@@ -1,7 +0,0 @@
-"""
-魔搭社区(https://modelscope.cn/home)下载大模型
-"""
-
-from modelscope import snapshot_download
-
-snapshot_download("LLM-Research/Meta-Llama-3-8B-Instruct-GGUF", cache_dir=".")
--- AI/NLP/README.md
+++
@@ -1,7 +0,0 @@
-# NLP
-
-Natuarl Language Processing 自然语言处理
-
-## 语句编码
-
-one-hot 独热编码
