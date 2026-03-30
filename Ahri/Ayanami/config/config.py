9d65f578aa7edd4fa8e1c328a232b3af0b6c64d0
Sokyoei <89757286+Sokyoei@users.noreply.github.com>
2026-03-30 17:34:20 +0800

    Ahri.Ayanami add config ml

---
+++ Ahri/Ayanami/config/config.py
@@ -0,0 +1,53 @@
+"""
+config form envioronment variables and .env file
+"""
+
+import sys
+from functools import lru_cache
+from pathlib import Path
+
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+from Ahri.Ayanami import AYANAMI_ROOT
+
+
+class Settings(BaseSettings):
+    DEBUG: bool = False
+
+    # dir
+    LOG_DIR: Path = AYANAMI_ROOT / "logs"
+    DOWNLOAD_DIR: Path = AYANAMI_ROOT / "downloads"
+    MODELS_DIR: Path = AYANAMI_ROOT / "models"
+    DATA_DIR: Path = AYANAMI_ROOT / "data"
+    VIDEOS_DIR: Path = AYANAMI_ROOT / "videos"
+    IMAGES_DIR: Path = AYANAMI_ROOT / "images"
+
+    # make dir
+    LOG_DIR.mkdir(parents=True, exist_ok=True)
+    DOWNLOAD_DIR.mkdir(parents=True, exist_ok=True)
+    MODELS_DIR.mkdir(parents=True, exist_ok=True)
+    DATA_DIR.mkdir(parents=True, exist_ok=True)
+    VIDEOS_DIR.mkdir(parents=True, exist_ok=True)
+    IMAGES_DIR.mkdir(parents=True, exist_ok=True)
+
+    model_config = SettingsConfigDict(
+        env_file=[AYANAMI_ROOT / ".env", AYANAMI_ROOT / ".env.dev", AYANAMI_ROOT / ".env.prod"],
+        env_file_encoding="utf-8",
+        extra="ignore",
+    )
+
+
+if sys.version_info >= (3, 8):
+
+    @lru_cache
+    def get_settings() -> Settings:
+        return Settings()
+
+else:
+
+    @lru_cache(128)
+    def get_settings() -> Settings:
+        return Settings()
+
+
+settings = get_settings()
